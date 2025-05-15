c3d6a1388f2f9f89064eb032367c46ced52d60ed
Aman Pratap Singh <amanpratapsingh@jklu.edu.in>
2025-05-15 16:35:42 +0530

    Your commit message here

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function openPopup(videoSrc) {
+    const popup = document.getElementById("videoPopup");
+    const popupVideo = document.getElementById("popupVideo");
+    popup.style.display = "flex";
+    popupVideo.src = videoSrc;
+}
+
+function closePopup() {
+    const popup = document.getElementById("videoPopup");
+    const popupVideo = document.getElementById("popupVideo");
+    popup.style.display = "none";
+    popupVideo.pause();
+    popupVideo.src = "";
+}
+
